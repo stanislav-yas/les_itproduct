7e3fe381375143d09fb28bd78a656878c4be5cc7
Stas <stanislav.yas@gmail.com>
2022-06-13 19:45:40 +0600

    Урок 3

---
+++ auth/lib/auth.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:conduit/conduit.dart';
+
+class AppService extends ApplicationChannel {
+  late final ManagedContext managedContext;
+
+  @override
+  Future prepare() {
+    PersistentStore persistentStore = _initDatabase();
+    managedContext = ManagedContext(
+        ManagedDataModel.fromCurrentMirrorSystem(), persistentStore);
+    return super.prepare();
+  }
+
+  @override
+  Controller get entryPoint => Router();
+
+  PostgreSQLPersistentStore _initDatabase() {
+    final username = Platform.environment["DB_USERNAME"] ?? "admin";
+    final password = Platform.environment["DB_PASSWORD"] ?? "root";
+    final host = Platform.environment["DB_HOST"] ?? "192.168.99.102";
+    final port = int.parse(Platform.environment["DB_PORT"] ?? "5432");
+    final databaseName = Platform.environment["DB_NAME"] ?? "postgres";
+    return PostgreSQLPersistentStore(
+        username, password, host, port, databaseName);
+  }
+}

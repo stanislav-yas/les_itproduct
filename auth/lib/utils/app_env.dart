02e2c1a268bf3733bc9777d0b9c80718c8521eaa
Stas <stanislav.yas@gmail.com>
2022-07-27 16:38:44 +0600

    Урок 19

--- auth/lib/utils/app_env.dart
+++ auth/lib/utils/app_env.dart
@@ -1,17 +1,11 @@
 import 'dart:io';
 
 abstract class AppEnv {
-  static final String secretKey =
-      Platform.environment["SECRET_KEY"] ?? "SECRET_KEY";
-
-  static final String port = Platform.environment["PORT"] ?? "8080";
-  static final String dbUsername =
-      Platform.environment["DB_USERNAME"] ?? "admin";
-  static final String dbPassword =
-      Platform.environment["DB_PASSWORD"] ?? "root";
-  static final String dbHost =
-      Platform.environment["DB_HOST"] ?? "192.168.99.101";
-  static final String dbPort = Platform.environment["DB_PORT"] ?? "6101";
-  static final String dbDatabaseName =
-      Platform.environment["DB_NAME"] ?? "postgres";
+  static final String secretKey = Platform.environment["SECRET_KEY"] ?? "";
+  static final String port = Platform.environment["PORT"] ?? "";
+  static final String dbUsername = Platform.environment["DB_USERNAME"] ?? "";
+  static final String dbPassword = Platform.environment["DB_PASSWORD"] ?? "";
+  static final String dbHost = Platform.environment["DB_HOST"] ?? "";
+  static final String dbPort = Platform.environment["DB_PORT"] ?? "";
+  static final String dbDatabaseName = Platform.environment["DB_NAME"] ?? "";
 }

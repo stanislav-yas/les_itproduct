e6ae292babe698660a1f4c7a4df7b7beef13fbb2
Stas <stanislav.yas@gmail.com>
2022-11-23 16:09:46 +0600

    Урок 5.3

--- client_it/lib/app/ui/main_app_runner.dart
+++ client_it/lib/app/ui/main_app_runner.dart
@@ -1,13 +1,17 @@
+import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/app/domain/app_builder.dart';
 import 'package:client_it/app/domain/app_runner.dart';
 import 'package:flutter/cupertino.dart';
 
 class MainAppRunner implements AppRunner {
+  final String env;
+
+  const MainAppRunner(this.env);
   @override
   Future<void> preloadData() async {
     WidgetsFlutterBinding.ensureInitialized();
     // init app
-    // init di
+    initDi(env);
     // init config
   }
 
--- client_it/lib/main.dart
+++ client_it/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:client_it/app/ui/main_app_builder.dart';
 import 'package:client_it/app/ui/main_app_runner.dart';
 
 void main() {
-  final runner = MainAppRunner();
+  const env = String.fromEnvironment("env", defaultValue: "dev");
+  const runner = MainAppRunner(env);
   final builder = MainAppBuilder();
   runner.run(builder);
 }

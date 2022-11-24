59778d131049f71edd4bd132fc69e700413c2b8c
Stas <stanislav.yas@gmail.com>
2022-11-24 16:49:50 +0600

    Урок 5.4

---
+++ client_it/lib/app/data/main_app_config.dart
@@ -0,0 +1,32 @@
+import 'package:client_it/app/domain/app_config.dart';
+import 'package:injectable/injectable.dart';
+
+@Singleton(as: AppConfig)
+@prod
+class ProdAppConfig implements AppConfig {
+  @override
+  String get baseUrl => "http://192.168.99.101/";
+
+  @override
+  String get host => throw Environment.prod;
+}
+
+@Singleton(as: AppConfig)
+@dev
+class DevAppConfig implements AppConfig {
+  @override
+  String get baseUrl => "localhost";
+
+  @override
+  String get host => throw Environment.dev;
+}
+
+@Singleton(as: AppConfig)
+@test
+class TestAppConfig implements AppConfig {
+  @override
+  String get baseUrl => "_";
+
+  @override
+  String get host => throw Environment.test;
+}
--- client_it/lib/app/di/init_di.config.dart
+++ client_it/lib/app/di/init_di.config.dart
@@ -6,9 +6,15 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
+import 'package:injectable/injectable.dart' as _i2;
 
+import '../data/main_app_config.dart' as _i4;
+import '../domain/app_config.dart' as _i3;
+
+const String _prod = 'prod';
+const String _dev = 'dev';
+const String _test = 'test';
+// ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(
@@ -20,6 +26,18 @@ _i1.GetIt $initGetIt(
     get,
     environment,
     environmentFilter,
+  );
+  gh.singleton<_i3.AppConfig>(
+    _i4.ProdAppConfig(),
+    registerFor: {_prod},
+  );
+  gh.singleton<_i3.AppConfig>(
+    _i4.DevAppConfig(),
+    registerFor: {_dev},
+  );
+  gh.singleton<_i3.AppConfig>(
+    _i4.TestAppConfig(),
+    registerFor: {_test},
   );
   return get;
 }

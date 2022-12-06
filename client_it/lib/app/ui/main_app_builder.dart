b761da2f546ea892d4de6166d23213c5b7e249fa
Stas <stanislav.yas@gmail.com>
2022-12-06 16:11:31 +0600

    Урок 5.11

--- client_it/lib/app/ui/main_app_builder.dart
+++ client_it/lib/app/ui/main_app_builder.dart
@@ -1,15 +1,38 @@
+import 'package:client_it/app/di/init_di.dart';
 import 'package:client_it/app/domain/app_builder.dart';
+import 'package:client_it/feature/auth/domain/auth_repository.dart';
+import 'package:client_it/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainAppBuilder implements AppBuilder {
   @override
   Widget buildApp() {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello"),
+    return const _GlobalProviders(
+      child: MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text("Hello"),
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _GlobalProviders extends StatelessWidget {
+  const _GlobalProviders({Key? key, required this.child}) : super(key: key);
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit(locator<AuthRepository>()),
+        )
+      ],
+      child: child,
     );
   }
 }
--- client_it/lib/feature/auth/domain/auth_state/auth_cubit.dart
+++ client_it/lib/feature/auth/domain/auth_state/auth_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:client_it/feature/auth/domain/auth_repository.dart';
 import 'package:client_it/feature/auth/domain/entities/user_entity/user_entity.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -6,5 +7,7 @@ part 'auth_state.dart';
 part 'auth_cubit.freezed.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthState.notAuthorized());
+  AuthCubit(this.authRepository) : super(AuthState.notAuthorized());
+
+  final AuthRepository authRepository;
 }
